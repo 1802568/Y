ecab75a13f37b359da57b8df02a0f01428ae7910
1802568 <76923976+1802568@users.noreply.github.com>
2021-05-06 14:59:36 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+const Engine = Engine;
+const World= World;
+const Bodies = Bodies;
+
+var engine, world;
+var ground
+var division
+function setup() {
+  var canvas = createCanvas(800,400);
+  engine = Engine.create();
+  world = engine.world;
+  ground = new Ground(600,height,1200,20);
+  division= new divisions(0,h,200,20)
+  division1= new divisions(200,h,200,20)
+  division2= new divisions(400,h,200,20)
+  division3= new divisions(600,h,200,20)
+  division4= new divisions(800,h,200,20)
+}
+
+function draw() {
+  background(0);
+  Engine.update(engine);
+ground.display()
+division.display()
+division1.display()
+division2.display()
+division3.display()
+division4.display()
+
+}
